e2f96942793b34b5583ac189dbd6129216832c20
Tony Han <h.bing612@gmail.com>
2016-08-23 01:45:14 +0800

    fix credentials bug in channel.c

--- src/grpc/channel.c
+++ src/grpc/channel.c
@@ -11,8 +11,10 @@ ERL_NIF_TERM nif_channel_create3(ErlNifEnv* env, int argc, const ERL_NIF_TERM ar
   wrapped_grpc_channel *wrapped_channel = enif_alloc_resource(grpc_channel_resource, sizeof(wrapped_grpc_channel));
   if (enif_is_atom(env, argv[2])) {
     char *target = NULL;
-    int res = better_get_atom(env, argv[2], &target);
-    if (res && strcmp(target, INSECURE_CRED_STRING) == 0) {
+    char *credentials = NULL;
+    int res1 = better_get_string(env, argv[0], &target);
+    int res2 = better_get_atom(env, argv[2], &credentials);
+    if (res1 && res2 && strcmp(credentials, INSECURE_CRED_STRING) == 0) {
       wrapped_channel->channel = grpc_insecure_channel_create(target, NULL, NULL);
     } else {
       return enif_raise_exception_compat(env, "The insecure cred must be atom :this_channel_is_insecure.");
