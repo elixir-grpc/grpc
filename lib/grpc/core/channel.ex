efb3f65ba36a7cb07ab26877a7ca1830f4cfe101
Tony Han <h.bing612@gmail.com>
2016-09-04 14:16:24 +0800

    add unary_call to send request and get response

--- lib/grpc/core/channel.ex
+++ lib/grpc/core/channel.ex
@@ -1,7 +1,12 @@
 defmodule GRPC.Core.Channel do
   import GRPC.Nifs
 
+  def create(target, channel_args, credentials) when is_binary(target) do
+    target = String.to_charlist(target)
+    create(target, channel_args, credentials)
+  end
   def create(target, channel_args, credentials) do
     channel_create(target, channel_args, credentials)
   end
+
 end
--- lib/grpc/stub.ex
+++ lib/grpc/stub.ex
@@ -1,12 +1,30 @@
 defmodule GRPC.Stub do
   defmacro __using__(opts) do
     quote bind_quoted: [service_mod: opts[:service]] do
+      service_name = service_mod.__meta__(:name)
+      marshal = service_mod.__meta__(:marshal_function)
+      unmarshal = service_mod.__meta__(:unmarshal_function)
       Enum.each service_mod.__rpc_calls__, fn ({name, request_mod, reply_mod}) ->
         func_name = name |> to_string |> Macro.underscore
-        def unquote(String.to_atom(func_name))(channel, request) do
-          apply(unquote(request_mod), unquote(service_mod.__meta__(:marshal_function)), [request])
+        route = "/#{service_name}/#{name}"
+        def unquote(String.to_atom(func_name))(channel, request, metadata \\ %{}) do
+          marshal_func = fn(req) -> apply(unquote(request_mod), unquote(marshal), [req]) end
+          unmarshal_func = fn(res) -> apply(unquote(request_mod), unquote(unmarshal), [res]) end
+          GRPC.Stub.unary_call(channel, unquote(route), request, marshal_func, unmarshal_func, metadata)
         end
       end
     end
+  end
+
+  def unary_call(channel, route, request, marshal_func, unmarshal_func, metadata, options \\ []) do
+    cq = GRPC.Core.CompletionQueue.create
+    deadline = :os.system_time(:seconds) + 300
+    call = GRPC.Core.Call.create(channel, nil, nil, cq, String.to_charlist(route), nil, deadline)
+
+    message = marshal_func.(request)
+    ops = %{0 => metadata, 1 => message, 2 => true, 4 => true, 5 => true, 6 => true}
+    stack = GRPC.Core.Call.run_batch(call, ops, nil)
+    result = GRPC.Core.Call.finish_batch(stack, cq, nil, 5)
+    Map.put(result, :message, unmarshal_func.(result[:message]))
   end
 end
