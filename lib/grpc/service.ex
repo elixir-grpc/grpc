bbe66184456b7359968d3b69f3ceae4f0b554fd7
Tony Han <h.bing612@gmail.com>
2016-09-30 00:28:20 +0800

    support stream param in Service

--- lib/grpc/service.ex
+++ lib/grpc/service.ex
@@ -1,7 +1,7 @@
 defmodule GRPC.Service do
   defmacro __using__(opts) do
     quote do
-      import GRPC.Service, only: [rpc: 3]
+      import GRPC.Service, only: [rpc: 3, stream: 1]
       @service_opts unquote(opts)
 
       Module.register_attribute(__MODULE__, :rpc_calls, accumulate: true)
@@ -18,7 +18,7 @@ defmodule GRPC.Service do
     end
 
     quote do
-      def __rpc_calls__, do: unquote(Macro.escape(rpc_calls))
+      def __rpc_calls__, do: unquote(rpc_calls |> Macro.escape |> Enum.reverse)
       def __meta__(:name), do: unquote(service_opts[:name])
       def __meta__(:marshal_function), do: unquote(service_opts[:marshal_function])
       def __meta__(:unmarshal_function), do: unquote(service_opts[:unmarshal_function])
@@ -27,8 +27,18 @@ defmodule GRPC.Service do
 
   defmacro rpc(name, request, reply) do
     quote do
-      @rpc_calls {unquote(name), unquote(request), unquote(reply)}
+      @rpc_calls {unquote(name), unquote(wrap_stream(request)), unquote(wrap_stream(reply))}
     end
   end
 
+  def stream(param) do
+    quote do: {unquote(param), true}
+  end
+
+  def wrap_stream({:stream, _, _} = param) do
+    quote do: unquote(param)
+  end
+  def wrap_stream(param) do
+    quote do: {unquote(param), false}
+  end
 end
