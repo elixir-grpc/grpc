aae2550b5107120416de76507230770e4abc69c5
jhchabran <jh@chabran.fr>
2017-12-10 12:12:13 +0100

    Add a server test for middlewares

--- test/grpc/server_test.exs
+++ test/grpc/server_test.exs
@@ -1,12 +1,27 @@
 defmodule GRPC.ServerTest do
   use ExUnit.Case, async: true
+  import ExUnit.CaptureIO
+
+  defmodule A do
+    def stream_send(stream, response, next) do
+      IO.write("A")
+      next.(stream, response)
+    end
+  end
+
+  defmodule B do
+    def stream_send(stream, response, next) do
+      IO.write("B")
+      next.(stream, response)
+    end
+  end
 
   defmodule Greeter.Service do
     use GRPC.Service, name: "hello"
   end
 
   defmodule Greeter.Server do
-    use GRPC.Server, service: Greeter.Service
+    use GRPC.Server, service: Greeter.Service, middlewares: [A, B]
   end
 
   test "stop/2 works" do
@@ -14,8 +29,10 @@ defmodule GRPC.ServerTest do
   end
 
   test "stream_send/2 works" do
-    stream = %GRPC.Server.Stream{adapter: GRPC.Test.ServerAdapter, marshal: &(&1)}
-    response = <<1, 2, 3, 4, 5, 6, 7, 8>>
-    assert {:ok, 13} == GRPC.Server.stream_send(stream, response)
+    assert capture_io(fn ->
+      stream = %GRPC.Server.Stream{adapter: GRPC.Test.ServerAdapter, marshal: &(&1)}
+      response = <<1, 2, 3, 4, 5, 6, 7, 8>>
+      assert {:ok, 13} == Greeter.Server.stream_send(stream, response)
+    end) == "BA"
   end
 end
