46202de74e77b8f879e73b2576e487cd8ee13e66
Chad Jablonski <chad@dinocore.net>
2017-12-17 10:31:57 -0800

    Add type to GRPC.RPCError

--- lib/grpc/error.ex
+++ lib/grpc/error.ex
@@ -1,5 +1,6 @@
 defmodule GRPC.RPCError do
   defexception [:status, :message]
+  @type t :: %__MODULE__{status: non_neg_integer, message: String.t}
 
   def exception(status, message) do
     %GRPC.RPCError{status: status, message: message}
