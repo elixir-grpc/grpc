1922b0192616a394372c24f59e4ff31b67a961c8
falood <falood@gmail.com>
2018-02-22 21:46:12 +0800

    fix unittest

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,10 @@
 Code.require_file("./support/test_adapter.exs", __DIR__)
+
+case Application.get_env(:grpc, :http2_client_adapter) do
+  GRPC.Adapter.Gun ->
+    Application.ensure_all_started(:gun)
+  GRPC.Adapter.Chatterbox.Client ->
+    Application.ensure_all_started(:chatterbox)
+end
+
 ExUnit.start()
