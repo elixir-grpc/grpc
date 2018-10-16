a646b7f3064c488df465c2ac46d4e0d71d738e22
Steve Cohen <scohen@discordapp.com>
2018-10-16 09:43:01 -0700

    Added disconnect
    From what I could tell, there was no way to disconnect a gun client,
    which was causing us to leak ports. This PR adds the ability to
    disconnect ourselves and clean up any open ports.

--- test/grpc/integration/stub_test.exs
+++ test/grpc/integration/stub_test.exs
@@ -17,6 +17,37 @@ defmodule GRPC.Integration.StubTest do
     end
   end
 
+  def port_for(pid) do
+    Port.list()
+    |> Enum.find(fn port ->
+      case Port.info(port, :links) do
+        {:links, links} ->
+          pid in links
+
+        _ ->
+          false
+      end
+    end)
+  end
+
+  test "you can disconnect stubs" do
+    run_server(HelloServer, fn port ->
+      port_count_before = :erlang.ports() |> Enum.count()
+      {:ok, channel} = GRPC.Stub.connect("localhost:#{port}")
+
+      %{adapter_payload: %{conn_pid: gun_conn_pid}} = channel
+
+      gun_port = port_for(gun_conn_pid)
+      Port.monitor(gun_port)
+
+      {:ok, channel} = GRPC.Stub.disconnect(channel)
+
+      assert %{adapter_payload: %{conn_pid: nil}} = channel
+      assert_receive {:DOWN, _, :port, ^gun_port, _}
+      assert port_for(gun_conn_pid) == nil
+    end)
+  end
+
   test "body larger than 2^14 works" do
     run_server(HelloServer, fn port ->
       {:ok, channel} = GRPC.Stub.connect("localhost:#{port}")
