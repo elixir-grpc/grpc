39205f962c0a8c7420b775bd3d9aa698ec376104
Jamie Hewland <jhewland@gmail.com>
2018-10-21 16:32:11 +0200

    Travis cleanups & updates (#76)
    * Travis: Run tests only on OTP 20+ and add Elixir 1.7 build
    
    * Update inch, don't run on Elixir 1.7, move to dev/test env
    
    * Fix Travis YAML
    
    * Travis: Try fix Elixir 1.7 check
    
    * Reorder Travis sections
    
    * Run mix format and add a Travis step to check formatted

--- lib/grpc/error.ex
+++ lib/grpc/error.ex
@@ -17,11 +17,12 @@ defmodule GRPC.RPCError do
 
   @spec exception(Status.t(), String.t()) :: t
   def new(status) when is_atom(status) do
-    exception([status: status])
+    exception(status: status)
   end
 
   def exception(args) when is_list(args) do
     error = parse_args(args, %__MODULE__{})
+
     if error.message do
       error
     else
@@ -30,15 +31,18 @@ defmodule GRPC.RPCError do
   end
 
   defp parse_args([], acc), do: acc
-  defp parse_args([{:status, status}|t], acc) when is_integer(status) do
+
+  defp parse_args([{:status, status} | t], acc) when is_integer(status) do
     acc = %{acc | status: status}
     parse_args(t, acc)
   end
-  defp parse_args([{:status, status}|t], acc) when is_atom(status) do
+
+  defp parse_args([{:status, status} | t], acc) when is_atom(status) do
     acc = %{acc | status: apply(GRPC.Status, status, [])}
     parse_args(t, acc)
   end
-  defp parse_args([{:message, message}|t], acc) when is_binary(message) do
+
+  defp parse_args([{:message, message} | t], acc) when is_binary(message) do
     acc = %{acc | message: message}
     parse_args(t, acc)
   end
@@ -47,20 +51,34 @@ defmodule GRPC.RPCError do
     %GRPC.RPCError{status: status, message: message}
   end
 
-  defp status_message(1),  do: "The operation was cancelled (typically by the caller)"
-  defp status_message(2),  do: "Unknown error"
-  defp status_message(3),  do: "Client specified an invalid argument"
-  defp status_message(4),  do: "Deadline expired before operation could complete"
-  defp status_message(5),  do: "Some requested entity (e.g., file or directory) was not found"
-  defp status_message(6),  do: "Some entity that we attempted to create (e.g., file or directory) already exists"
-  defp status_message(7),  do: "The caller does not have permission to execute the specified operation"
-  defp status_message(8),  do: "Some resource has been exhausted"
-  defp status_message(9),  do: "Operation was rejected because the system is not in a state required for the operation's execution"
+  defp status_message(1), do: "The operation was cancelled (typically by the caller)"
+  defp status_message(2), do: "Unknown error"
+  defp status_message(3), do: "Client specified an invalid argument"
+  defp status_message(4), do: "Deadline expired before operation could complete"
+  defp status_message(5), do: "Some requested entity (e.g., file or directory) was not found"
+
+  defp status_message(6),
+    do: "Some entity that we attempted to create (e.g., file or directory) already exists"
+
+  defp status_message(7),
+    do: "The caller does not have permission to execute the specified operation"
+
+  defp status_message(8), do: "Some resource has been exhausted"
+
+  defp status_message(9),
+    do:
+      "Operation was rejected because the system is not in a state required for the operation's execution"
+
   defp status_message(10), do: "The operation was aborted"
   defp status_message(11), do: "Operation was attempted past the valid range"
-  defp status_message(12), do: "Operation is not implemented or not supported/enabled in this service"
+
+  defp status_message(12),
+    do: "Operation is not implemented or not supported/enabled in this service"
+
   defp status_message(13), do: "Internal errors"
   defp status_message(14), do: "The service is currently unavailable"
   defp status_message(15), do: "Unrecoverable data loss or corruption"
-  defp status_message(16), do: "The request does not have valid authentication credentials for the operation"
+
+  defp status_message(16),
+    do: "The request does not have valid authentication credentials for the operation"
 end
