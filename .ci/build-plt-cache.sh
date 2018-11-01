3f9769bbfed6668a17f343fb536f3887c1d767d0
Jamie Hewland <jhewland@gmail.com>
2018-11-01 13:15:42 +0200

    build-plt-cache.sh: Simplify some things

--- .ci/build-plt-cache.sh
+++ .ci/build-plt-cache.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 # This script builds/restores PLT cache files to/from a cache at ~/.pltcache
-set -e
-
-if [[ "$TRAVIS" != 'true' ]]; then
-  echo 'This script should only be used in a Travis CI build.' 1>&2
-  exit 1
-fi
+set -ex
 
 # "Mix will default to the :dev environment"
 MIX_ENV="${MIX_ENV:-dev}"
-
-set -x
 
 mix compile
 
