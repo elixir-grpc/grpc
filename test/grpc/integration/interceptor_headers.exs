14408d6bd20cdee56d3b24378234c01a2d523516
Mairbek Khadikov <mairbek@brex.com>
2019-11-01 18:44:35 -0700

    Allow client interceptor to send headers

---
+++ test/grpc/integration/interceptor_headers.exs
@@ -0,0 +1,60 @@
+defmodule GRPC.Integration.EndpointTest do
+  use GRPC.Integration.TestCase
+
+  defmodule HelloServer do
+    use GRPC.Server, service: Helloworld.Greeter.Service
+
+    def say_hello(req, _stream) do
+      Helloworld.HelloReply.new(message: "Hello, #{req.name}")
+    end
+  end
+
+  defmodule AddHeadersClientInterceptor do
+    def init(label), do: label
+
+    def call(%{headers: headers} = stream, req, next, label) do
+      new_headers =
+        case Map.get(headers, "x-test-label") do
+          nil -> %{"x-test-label" => label}
+          original -> %{headers | "x-test-label" => "#{original} #{label}"}
+        end
+
+      new_stream = GRPC.Client.Stream.put_headers(stream, new_headers)
+      next.(new_stream, req)
+    end
+  end
+
+  defmodule HeadersServer do
+    def init(_), do: []
+
+    def call(req, stream, next, _) do
+      headers = GRPC.Stream.get_headers(stream)
+      %{name: original_name} = req
+      label = headers["x-test-label"]
+      updated = Helloworld.HelloRequest.new(name: "#{original_name} #{label}")
+      next.(updated, stream)
+    end
+  end
+
+  defmodule HelloEndpoint do
+    use GRPC.Endpoint
+
+    run HelloServer, interceptors: [HeadersServer]
+  end
+
+  test "client sends headers" do
+    run_endpoint(HelloEndpoint, fn port ->
+      {:ok, channel} =
+        GRPC.Stub.connect("localhost:#{port}",
+          interceptors: [
+            {AddHeadersClientInterceptor, "two"},
+            {AddHeadersClientInterceptor, "one"}
+          ]
+        )
+
+      req = Helloworld.HelloRequest.new(name: "Elixir")
+      {:ok, reply} = channel |> Helloworld.Greeter.Stub.say_hello(req)
+      assert reply.message == "Hello, Elixir one two"
+    end)
+  end
+end
