98f9ddc87c3b98e0b9cb8571df70f6842157d099
Stanislav Zahariev <stanislav.zahariev@sumup.com>
2019-11-08 14:17:54 +0200

    Add ability to retreive the peer

--- lib/grpc/adapter/cowboy.ex
+++ lib/grpc/adapter/cowboy.ex
@@ -132,6 +132,10 @@ defmodule GRPC.Adapter.Cowboy do
     Handler.get_headers(pid)
   end
 
+  def get_peer(%{pid: pid}) do
+    Handler.get_peer(pid)
+  end
+
   def set_compressor(%{pid: pid}, compressor) do
     Handler.set_compressor(pid, compressor)
   end
