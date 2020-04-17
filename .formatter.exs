617d36417cd68b6da920f23f88277f9841dd59ea
Ethan Ransom <derethanhausen@gmail.com>
2020-04-17 13:23:04 -0600

    Accidentally a comma

--- .formatter.exs
+++ .formatter.exs
@@ -4,6 +4,6 @@
   import_deps: [:protobuf],
   locals_without_parens: [rpc: 3, intercept: 1, intercept: 2, run: 1, run: 2, field: 2, field: 3],
   export: [
-    locals_without_parens: [rpc: 3, intercept: 1, intercept: 2, run: 1, run: 2, field: 2, field, 3]
+    locals_without_parens: [rpc: 3, intercept: 1, intercept: 2, run: 1, run: 2, field: 2, field: 3]
   ]
 ]
