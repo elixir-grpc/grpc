59acf797860691591fbf4b53529684573690fd42
Peter Krenn <peter.krenn@gmail.com>
2021-05-28 13:45:49 +0200

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -36,11 +36,9 @@ defmodule GRPC.Mixfile do
 
   defp deps do
     [
-      {:protobuf, "~> 0.5"},
-      {:cowboy, "~> 2.7"},
-      {:gun, "~> 2.0.0", hex: :grpc_gun},
-      # 2.9.0 fixes some important bugs, so it's better to use ~> 2.9.0
-      # {:cowlib, "~> 2.9.0", override: true},
+      {:protobuf, "~> 0.7"},
+      {:cowboy, "~> 2.9"},
+      {:gun, "~> 2.0.0-rc.2"},
       {:ex_doc, "~> 0.23", only: :dev},
       {:inch_ex, "~> 2.0", only: [:dev, :test]},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
