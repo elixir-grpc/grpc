6b162a56ce357b632c16ccf7b7cbff85320dbfb6
taotao.lin <cnredink@gmail.com>
2021-08-16 14:02:35 +0800

    [fix] fix erlang:get_stacktrace

--- src/grpc_stream_h.erl
+++ src/grpc_stream_h.erl
@@ -329,17 +329,15 @@ request_process(Req, Env, Middlewares) ->
 	try
 		execute(Req, Env, Middlewares)
 	catch
-		exit:Reason ->
-			Stacktrace = erlang:get_stacktrace(),
+		exit:Reason:Stacktrace ->
 			erlang:raise(exit, {Reason, Stacktrace}, Stacktrace);
 		%% OTP 19 does not propagate any exception stacktraces,
 		%% we therefore add it for every class of exception.
-		_:Reason when OTP =:= "19" ->
-			Stacktrace = erlang:get_stacktrace(),
+		_:Reason:Stacktrace when OTP =:= "19" ->
 			erlang:raise(exit, {Reason, Stacktrace}, Stacktrace);
 		%% @todo I don't think this clause is necessary.
-		Class:Reason ->
-			erlang:raise(Class, Reason, erlang:get_stacktrace())
+		Class:Reason:Stacktrace ->
+			erlang:raise(Class, Reason, Stacktrace)
 	end.
 
 execute(_, _, []) ->
