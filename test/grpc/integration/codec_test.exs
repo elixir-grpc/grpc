21b441f1c8f644961c99a55f3bcdc4279a4336d0
Simon Thörnqvist <simon.thornqvist@erlang-solutions.com>
2021-09-02 09:47:09 +0200

    include web-text in codec test

--- test/grpc/integration/codec_test.exs
+++ test/grpc/integration/codec_test.exs
@@ -8,6 +8,10 @@ defmodule GRPC.Integration.CodecTest do
       "not-registered"
     end
 
+    def pack_encoded(binary), do: binary
+
+    def prepare_decode(binary), do: binary
+
     def encode(struct) do
       :erlang.term_to_binary(struct)
     end
@@ -20,32 +24,35 @@ defmodule GRPC.Integration.CodecTest do
   defmodule HelloServer do
     use GRPC.Server,
       service: Helloworld.Greeter.Service,
-      codecs: [GRPC.Codec.Proto, GRPC.Codec.Erlpack]
+      codecs: [GRPC.Codec.Proto, GRPC.Codec.Erlpack, GRPC.Codec.WebText]
 
     def say_hello(req, _stream) do
       Helloworld.HelloReply.new(message: "Hello, #{req.name}")
     end
   end
 
-  defmodule HelloErlpackStub do
+  defmodule HelloStub do
     use GRPC.Stub, service: Helloworld.Greeter.Service
   end
 
-  test "Says hello over erlpack" do
+  test "Says hello over erlpack, GRPC-web-text" do
     run_server(HelloServer, fn port ->
       {:ok, channel} = GRPC.Stub.connect("localhost:#{port}")
       name = "Mairbek"
       req = Helloworld.HelloRequest.new(name: name)
 
-      {:ok, reply} = channel |> HelloErlpackStub.say_hello(req, codec: GRPC.Codec.Erlpack)
+      {:ok, reply} = channel |> HelloStub.say_hello(req, codec: GRPC.Codec.Erlpack)
+      assert reply.message == "Hello, #{name}"
+
+      {:ok, reply} = channel |> HelloStub.say_hello(req, codec: GRPC.Codec.WebText)
       assert reply.message == "Hello, #{name}"
 
       # verify that proto still works
-      {:ok, reply} = channel |> HelloErlpackStub.say_hello(req, codec: GRPC.Codec.Proto)
+      {:ok, reply} = channel |> HelloStub.say_hello(req, codec: GRPC.Codec.Proto)
       assert reply.message == "Hello, #{name}"
 
       # codec not registered
-      {:error, reply} = channel |> HelloErlpackStub.say_hello(req, codec: NotRegisteredCodec)
+      {:error, reply} = channel |> HelloStub.say_hello(req, codec: NotRegisteredCodec)
 
       assert %GRPC.RPCError{
                status: GRPC.Status.unimplemented(),
