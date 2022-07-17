d228f8bed884cc9e87033e9d62f56b9fe5fde60b
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2022-07-16 21:38:45 -0300

    chore: update elixir and erlang

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule GRPC.Mixfile do
     [
       app: :grpc,
       version: @version,
-      elixir: "~> 1.5",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
