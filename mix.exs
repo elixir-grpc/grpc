a86ba82817dbb49e312a3e5355a10662e17e30f9
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2022-07-16 21:17:47 -0300

    chore: update deps

--- mix.exs
+++ mix.exs
@@ -36,14 +36,14 @@ defmodule GRPC.Mixfile do
 
   defp deps do
     [
-      {:protobuf, "~> 0.5"},
-      {:cowboy, "~> 2.7"},
+      # TO-DO: update protobuf to 0.10 (has breaking changes)
+      {:protobuf, "~> 0.9.0"},
+      {:cowboy, "~> 2.9"},
       {:gun, "~> 2.0.0", hex: :grpc_gun},
-      # 2.9.0 fixes some important bugs, so it's better to use ~> 2.9.0
-      # {:cowlib, "~> 2.9.0", override: true},
-      {:ex_doc, "~> 0.23", only: :dev},
+      {:cowlib, "~> 2.11", override: true},
+      {:ex_doc, "~> 0.28", only: :dev},
       {:inch_ex, "~> 2.0", only: [:dev, :test]},
-      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false}
     ]
   end
 
