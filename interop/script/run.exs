80199cc260dc17553055ffb4450431ccebb62c4e
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2022-07-23 09:36:03 -0300

    fix: supervisor call

--- interop/script/run.exs
+++ interop/script/run.exs
@@ -18,7 +18,7 @@ alias Interop.Client
 
 stream = Task.async_stream(1..concurrency, fn _cli ->
   ch = Client.connect("127.0.0.1", port, interceptors: [GRPCPrometheus.ClientInterceptor, GRPC.Logger.Client])
-  
+
   for _ <- 1..rounds do
     Client.empty_unary!(ch)
     Client.cacheable_unary!(ch)
