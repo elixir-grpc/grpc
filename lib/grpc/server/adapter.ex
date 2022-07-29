0c3bb127c3b3828f8b78ff437f826ca81b65355f
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2022-07-29 10:23:42 -0300

    fix: dialyzer

--- lib/grpc/server/adapter.ex
+++ lib/grpc/server/adapter.ex
@@ -3,6 +3,14 @@ defmodule GRPC.Server.Adapter do
   HTTP server adapter for GRPC.
   """
 
+  @type state :: %{
+          pid: pid,
+          handling_timer: reference | nil,
+          resp_trailers: map,
+          compressor: atom | nil,
+          pending_reader: nil
+        }
+
   @callback start(
               atom(),
               %{String.t() => [module()]},
@@ -13,7 +21,7 @@ defmodule GRPC.Server.Adapter do
 
   @callback stop(atom(), %{String.t() => [module()]}) :: :ok | {:error, :not_found}
 
-  @callback send_reply(Handler.state(), content :: binary(), opts :: keyword()) :: any()
+  @callback send_reply(state, content :: binary(), opts :: keyword()) :: any()
 
-  @callback send_headers(Handler.state(), headers :: map()) :: any()
+  @callback send_headers(state, headers :: map()) :: any()
 end
