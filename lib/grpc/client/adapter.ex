4764228f326868996c6c814c183881535ecceb6e
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2022-07-29 00:38:29 -0300

    docs: add variable names

--- lib/grpc/client/adapter.ex
+++ lib/grpc/client/adapter.ex
@@ -9,15 +9,21 @@ defmodule GRPC.Client.Adapter do
   @typedoc "Determines if the headers have finished being read."
   @type fin :: :fin | :nofin
 
-  @callback connect(Channel.t(), keyword()) :: {:ok, Channel.t()} | {:error, any()}
+  @callback connect(channel :: Channel.t(), opts :: keyword()) ::
+              {:ok, Channel.t()} | {:error, any()}
 
-  @callback disconnect(Channel.t()) :: {:ok, Channel.t()} | {:error, any()}
+  @callback disconnect(channel :: Channel.t()) :: {:ok, Channel.t()} | {:error, any()}
 
-  @callback send_request(Stream.t(), binary(), keyword()) :: Stream.t()
+  @callback send_request(stream :: Stream.t(), contents :: binary(), opts :: keyword()) ::
+              Stream.t()
 
-  @callback recv_headers(map(), map(), keyword()) ::
+  @callback recv_headers(stream :: map(), headers :: map(), opts :: keyword()) ::
               {:ok, %{String.t() => String.t()}, fin()} | {:error, GRPC.RPCError.t()}
 
-  @callback recv_data_or_trailers(map(), map(), keyword()) ::
+  @callback recv_data_or_trailers(
+              stream :: map(),
+              trailers_or_metadata :: map(),
+              opts :: keyword()
+            ) ::
               {:data, binary()} | {:trailers, binary()} | {:error, GRPC.RPCError.t()}
 end
