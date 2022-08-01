20affcc09a889493a3a7b0aa4579d03f93e37494
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2022-08-01 00:39:48 -0300

    fix: add if-compiled

--- lib/grpc/client/adapters/mint/connection.ex
+++ lib/grpc/client/adapters/mint/connection.ex
@@ -1,10 +1,12 @@
-defmodule GRPC.Client.Adapters.Mint do
-  @moduledoc """
-  Connection metadata that define the `adapter_payload` field for a Mint-controlled `GRPC.Channel`
-  """
-  defstruct [
-    :conn,
-    requests: %{},
-    keep_alive_timer: %GRPC.Client.Adapters.Mint.KeepAliveTimer{}
-  ]
+if {:module, Mint} == Code.ensure_compiled(Mint) do
+  defmodule GRPC.Client.Adapters.Mint do
+    @moduledoc """
+    Connection metadata that define the `adapter_payload` field for a Mint-controlled `GRPC.Channel`
+    """
+    defstruct [
+      :conn,
+      requests: %{},
+      keep_alive_timer: %GRPC.Client.Adapters.Mint.KeepAliveTimer{}
+    ]
+  end
 end
--- lib/grpc/client/adapters/mint/keep_alive_timer.ex
+++ lib/grpc/client/adapters/mint/keep_alive_timer.ex
@@ -1,67 +1,69 @@
-defmodule GRPC.Client.Adapters.Mint.KeepAliveTimer do
-  # This file is a copy of https://github.com/NFIBrokerage/spear/blob/9177196721d943fda2ee1a70698580b036f220ee/lib/spear/connection/keep_alive_timer.ex
-  @moduledoc """
-  Curates the keep-alive timer for a Mint-controlled channel connection.
+if {:module, Mint} == Code.ensure_compiled(Mint) do
+  defmodule GRPC.Client.Adapters.Mint.KeepAliveTimer do
+    # This file is a copy of https://github.com/NFIBrokerage/spear/blob/9177196721d943fda2ee1a70698580b036f220ee/lib/spear/connection/keep_alive_timer.ex
+    @moduledoc """
+    Curates the keep-alive timer for a Mint-controlled channel connection.
 
-  gRPC keep-alive is just HTTP2 keep-alive - send PING frames every interval
-  after receiving some data and wait at most timeout before saying the
-  connection is severed
+    gRPC keep-alive is just HTTP2 keep-alive - send PING frames every interval
+    after receiving some data and wait at most timeout before saying the
+    connection is severed
 
-  note that `:interval_timer` is not an actual interval timer as with
-  `:timer.send_interval/3`. See https://github.com/NFIBrokerage/spear/issues/10
-  for info on keep-alive conformity
-  """
+    note that `:interval_timer` is not an actual interval timer as with
+    `:timer.send_interval/3`. See https://github.com/NFIBrokerage/spear/issues/10
+    for info on keep-alive conformity
+    """
 
-  defstruct interval_timer: nil, interval: 10_000, timeout: 10_000, timeout_timers: %{}
+    defstruct interval_timer: nil, interval: 10_000, timeout: 10_000, timeout_timers: %{}
 
-  def start(%{keep_alive_interval: interval, keep_alive_timeout: timeout})
-      when interval == false or timeout == false,
-      do: %__MODULE__{}
+    def start(%{keep_alive_interval: interval, keep_alive_timeout: timeout})
+        when interval == false or timeout == false,
+        do: %__MODULE__{}
 
-  def start(%{keep_alive_interval: interval, keep_alive_timeout: timeout}) do
-    %__MODULE__{
-      interval_timer: start_interval_timer(interval),
-      interval: interval,
-      timeout: timeout
-    }
-  end
+    def start(%{keep_alive_interval: interval, keep_alive_timeout: timeout}) do
+      %__MODULE__{
+        interval_timer: start_interval_timer(interval),
+        interval: interval,
+        timeout: timeout
+      }
+    end
 
-  def reset_interval_timer(%__MODULE__{} = keep_alive_timer) do
-    cancel_timer(keep_alive_timer.interval_timer)
+    def reset_interval_timer(%__MODULE__{} = keep_alive_timer) do
+      cancel_timer(keep_alive_timer.interval_timer)
 
-    %__MODULE__{
+      %__MODULE__{
+        keep_alive_timer
+        | interval_timer: start_interval_timer(keep_alive_timer.interval)
+      }
+    end
+
+    def clear(%__MODULE__{interval_timer: interval_timer, timeout_timers: timeout_timers}) do
+      cancel_timer(interval_timer)
+
+      :ok = Enum.each(timeout_timers, fn {_request_ref, timer} -> cancel_timer(timer) end)
+
+      %__MODULE__{}
+    end
+
+    def start_timeout_timer(%__MODULE__{} = keep_alive_timer, request_ref) do
+      put_in(
+        keep_alive_timer.timeout_timers[request_ref],
+        Process.send_after(self(), :keep_alive_expired, keep_alive_timer.timeout)
+      )
+    end
+
+    def clear_after_timer(%__MODULE__{} = keep_alive_timer, request_ref) do
+      {timeout_timer, keep_alive_timer} = pop_in(keep_alive_timer.timeout_timers[request_ref])
+
+      cancel_timer(timeout_timer)
+
       keep_alive_timer
-      | interval_timer: start_interval_timer(keep_alive_timer.interval)
-    }
+    end
+
+    defp start_interval_timer(interval) do
+      Process.send_after(self(), :keep_alive, interval)
+    end
+
+    defp cancel_timer(nil), do: :ok
+    defp cancel_timer(timer), do: Process.cancel_timer(timer)
   end
-
-  def clear(%__MODULE__{interval_timer: interval_timer, timeout_timers: timeout_timers}) do
-    cancel_timer(interval_timer)
-
-    :ok = Enum.each(timeout_timers, fn {_request_ref, timer} -> cancel_timer(timer) end)
-
-    %__MODULE__{}
-  end
-
-  def start_timeout_timer(%__MODULE__{} = keep_alive_timer, request_ref) do
-    put_in(
-      keep_alive_timer.timeout_timers[request_ref],
-      Process.send_after(self(), :keep_alive_expired, keep_alive_timer.timeout)
-    )
-  end
-
-  def clear_after_timer(%__MODULE__{} = keep_alive_timer, request_ref) do
-    {timeout_timer, keep_alive_timer} = pop_in(keep_alive_timer.timeout_timers[request_ref])
-
-    cancel_timer(timeout_timer)
-
-    keep_alive_timer
-  end
-
-  defp start_interval_timer(interval) do
-    Process.send_after(self(), :keep_alive, interval)
-  end
-
-  defp cancel_timer(nil), do: :ok
-  defp cancel_timer(timer), do: Process.cancel_timer(timer)
 end
