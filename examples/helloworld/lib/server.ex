56b8cfb76d233f2a6ced67fe54b8f8ca4e30a0e8
Angelo Moreira <angelo.moreira@withwise.com>
2022-08-05 23:05:53 +0100

    better time calculations based on `system_time`

--- examples/helloworld/lib/server.ex
+++ examples/helloworld/lib/server.ex
@@ -4,9 +4,9 @@ defmodule Helloworld.Greeter.Server do
   @spec say_hello(Helloworld.HelloRequest.t(), GRPC.Server.Stream.t()) ::
           Helloworld.HelloReply.t()
   def say_hello(request, _stream) do
-    today = DateTime.utc_now()
-    seconds = DateTime.truncate(today, :second) |> DateTime.to_unix(:second)
-    nanos = (DateTime.to_unix(today, :millisecond) |> rem(1000)) * 1_000_000
+    nanos_epoch = System.system_time() |> System.convert_time_unit(:native, :nanosecond)
+    seconds = div(nanos_epoch, 1_000_000_000)
+    nanos = nanos_epoch - seconds * 1_000_000_000
 
     Helloworld.HelloReply.new(
       message: "Hello #{request.name}",
