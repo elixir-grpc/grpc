fb1331667f201f768c88fd2511ccb3ba3d5211a7
Simon Thörnqvist <simon.thornqvist@erlang-solutions.com>
2022-09-12 16:39:56 +0200

    update helloworld example with HttpRule options

--- examples/helloworld/lib/helloworld.pb.ex
+++ examples/helloworld/lib/helloworld.pb.ex
@@ -23,3 +23,19 @@ defmodule Helloworld.HelloReply do
   field :message, 1, type: :string
   field :today, 2, type: Google.Protobuf.Timestamp
 end
+
+defmodule Helloworld.GetMessageRequest do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+
+  field :name, 1, type: :string
+end
+
+defmodule Helloworld.Message do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+
+  field :text, 1, type: :string
+end
--- examples/helloworld/lib/helloworld.svc.ex
+++ examples/helloworld/lib/helloworld.svc.ex
@@ -37,3 +37,29 @@ defmodule Helloworld.Greeter.Stub do
 
   use GRPC.Stub, service: Helloworld.Greeter.Service
 end
+
+defmodule Helloworld.Messaging.Service do
+  @moduledoc false
+
+  use GRPC.Service, name: "helloworld.Messaging", protoc_gen_elixir_version: "0.11.0"
+
+  rpc(:GetMessage, Helloworld.GetMessageRequest, Helloworld.Message, %{
+    http: %{
+      type: Google.Api.PbExtension,
+      value: %Google.Api.HttpRule{
+        __unknown_fields__: [],
+        additional_bindings: [],
+        body: "",
+        pattern: {:get, "/v1/{name=messages/*}"},
+        response_body: "",
+        selector: ""
+      }
+    }
+  })
+end
+
+defmodule Helloworld.Messaging.Stub do
+  @moduledoc false
+
+  use GRPC.Stub, service: Helloworld.Messaging.Service
+end
