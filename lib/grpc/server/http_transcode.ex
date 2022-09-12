1248ab20d4687bc8aaf7a8e1381add53ea106e1a
Simon Thörnqvist <simon.thornqvist@erlang-solutions.com>
2022-09-12 16:39:12 +0200

    wip! build route from HttpRule

--- lib/grpc/server/http_transcode.ex
+++ lib/grpc/server/http_transcode.ex
@@ -8,4 +8,104 @@ defmodule GRPC.Server.HTTPTranscode do
   def method(%{pattern: {method, _path}}) do
     method
   end
+
+  @doc """
+  https://cloud.google.com/endpoints/docs/grpc-service-config/reference/rpc/google.api#google.api.HttpRule
+
+  Template = "/" Segments [ Verb ] ;
+  Segments = Segment { "/" Segment } ;
+  Segment  = "*" | "**" | LITERAL | Variable ;
+  Variable = "{" FieldPath [ "=" Segments ] "}" ;
+  FieldPath = IDENT { "." IDENT } ;
+  Verb     = ":" LITERAL ;
+  """
+  @spec build_route(term()) :: tuple()
+  def build_route(%Google.Api.HttpRule{pattern: {method, path}}) do
+    route =
+      path
+      |> tokenize([])
+      |> parse([], [])
+
+    {method, route}
+  end
+
+  @spec tokenize(binary(), list()) :: list()
+  def tokenize(path, tokens \\ [])
+
+  def tokenize(<<>>, tokens) do
+    Enum.reverse(tokens)
+  end
+
+  def tokenize(segments, tokens) do
+    {token, rest} = do_tokenize(segments, <<>>)
+    tokenize(rest, [token | tokens])
+  end
+
+  @terminals [?/, ?{, ?}, ?=, ?*]
+  defp do_tokenize(<<h, t::binary>>, <<>>) when h in @terminals do
+    # parse(t, acc)
+    {{List.to_atom([h]), []}, t}
+  end
+
+  defp do_tokenize(<<h, _::binary>> = rest, acc) when h in @terminals do
+    {{:literal, acc, []}, rest}
+  end
+
+  defp do_tokenize(<<h, t::binary>>, acc)
+       when h in ?a..?z or h in ?A..?Z or h in ?0..?9 or h == ?_ or h == ?. do
+    do_tokenize(t, <<acc::binary, h>>)
+  end
+
+  defp do_tokenize(<<>>, acc) do
+    {{:literal, acc, []}, <<>>}
+  end
+
+  @spec parse(list(tuple()), list(), list()) :: list()
+  def parse([], params, segments) do
+    {Enum.reverse(params), Enum.reverse(segments)}
+  end
+
+  def parse([{:/, _} | rest], params, segments) do
+    parse(rest, params, segments)
+  end
+
+  def parse([{:*, _} | rest], params, segments) do
+    parse(rest, params, [{:_, []} | segments])
+  end
+
+  def parse([{:literal, literal, _} | rest], params, segments) do
+    parse(rest, params, [literal | segments])
+  end
+
+  def parse([{:"{", _} | rest], params, segments) do
+    {params, segments, rest} = parse_binding(rest, params, segments)
+    parse(rest, params, segments)
+  end
+
+  defp parse_binding([{:"}", []} | rest], params, segments) do
+    {params, segments, rest}
+  end
+
+  defp parse_binding(
+         [{:literal, lit, _}, {:=, _}, {:literal, assign, _} = a | rest],
+         params,
+         segments
+       ) do
+
+    {variable, _} = param = field_path(lit)
+    # assign = field_path(assign)
+
+    parse_binding(rest, [param | params], [{variable, [assign]} | segments])
+  end
+
+  defp parse_binding([{:literal, lit, []} | rest], params, segments) do
+    {variable, _} = param = field_path(lit)
+    parse_binding(rest, [param | params], [{variable, []} | segments])
+  end
+
+  def field_path(identifier) do
+    [root | path] = String.split(identifier, ".")
+    {String.to_atom(root), path}
+  end
+
 end
---
+++ test/grpc/transcode_test.exs
@@ -0,0 +1,148 @@
+defmodule GRPC.TranscodeTest do
+  use ExUnit.Case, async: true
+  alias GRPC.Server.HTTPTranscode, as: Transcode
+
+  describe "build_route/1" do
+    test "returns a route with {http_method, route} based on the http rule" do
+      rule = build_simple_rule(:get, "/v1/messages/{message_id}")
+      assert {:get, {params, segments}} = Transcode.build_route(rule)
+      assert [message_id: []] == params
+      assert ["v1", "messages", {:message_id, []}] = segments
+    end
+  end
+
+  describe "tokenize/2" do
+    test "can tokenize simple paths" do
+      assert [{:/, []}] = Transcode.tokenize("/")
+
+      assert [{:/, []}, {:literal, "v1", []}, {:/, []}, {:literal, "messages", []}] =
+               Transcode.tokenize("/v1/messages")
+    end
+
+    test "can tokenize simple paths with wildcards" do
+      assert [
+               {:/, []},
+               {:literal, "v1", []},
+               {:/, []},
+               {:literal, "messages", []},
+               {:/, []},
+               {:*, []}
+             ] == Transcode.tokenize("/v1/messages/*")
+    end
+
+    test "can tokenize simple variables" do
+      assert [
+               {:/, []},
+               {:literal, "v1", []},
+               {:/, []},
+               {:literal, "messages", []},
+               {:/, []},
+               {:"{", []},
+               {:literal, "message_id", []},
+               {:"}", []}
+             ] == Transcode.tokenize("/v1/messages/{message_id}")
+    end
+
+    test "can tokenize variable assignments in bindings" do
+      assert [
+               {:/, []},
+               {:literal, "v1", []},
+               {:/, []},
+               {:"{", []},
+               {:literal, "name", []},
+               {:=, []},
+               {:literal, "messages", []},
+               {:"}", []}
+             ] == Transcode.tokenize("/v1/{name=messages}")
+    end
+
+    test "can tokenize field paths in bindings" do
+      assert [
+               {:/, []},
+               {:literal, "v1", []},
+               {:/, []},
+               {:literal, "messages", []},
+               {:/, []},
+               {:"{", []},
+               {:literal, "message_id", []},
+               {:"}", []},
+               {:/, []},
+               {:"{", []},
+               {:literal, "sub.subfield", []},
+               {:"}", []}
+             ] == Transcode.tokenize("/v1/messages/{message_id}/{sub.subfield}")
+    end
+  end
+
+  describe "parse/3" do
+    test "can parse simple paths" do
+      assert {[], []} ==
+               "/"
+               |> Transcode.tokenize()
+               |> Transcode.parse([], [])
+    end
+
+    test "can parse paths with literals" do
+      assert {[], ["v1", "messages"]} ==
+               "/v1/messages"
+               |> Transcode.tokenize()
+               |> Transcode.parse([], [])
+    end
+
+    test "can parse paths with wildcards" do
+      assert {[], ["v1", "messages", {:_, []}]} ==
+               "/v1/messages/*"
+               |> Transcode.tokenize()
+               |> Transcode.parse([], [])
+    end
+
+    test "can parse simple bindings with variables" do
+      assert {[{:message_id, []}], ["v1", "messages", {:message_id, []}]} ==
+               "/v1/messages/{message_id}"
+               |> Transcode.tokenize()
+               |> Transcode.parse([], [])
+    end
+
+    test "can parse bindings with variable assignment" do
+      assert {[{:name, []}], ["v1", {:name, ["messages"]}]} ==
+               "/v1/{name=messages}"
+               |> Transcode.tokenize()
+               |> Transcode.parse([], [])
+    end
+
+    test "can parse multiple bindings with variable assignment" do
+      assert {[{:name, []}, {:message_id, []}], ["v1", {:name, ["messages"]}, {:message_id, []}]} ==
+               "/v1/{name=messages}/{message_id}"
+               |> Transcode.tokenize()
+               |> Transcode.parse([], [])
+    end
+
+    test "can parse bindings with field paths " do
+      assert {[sub: ["subfield"]], ["v1", "messages", {:sub, []}]} ==
+               "/v1/messages/{sub.subfield}"
+               |> Transcode.tokenize()
+               |> Transcode.parse([], [])
+    end
+
+    test "supports deeper nested field path " do
+      assert {[sub: ["nested", "nested", "nested"]], ["v1", "messages", {:sub, []}]} ==
+               "/v1/messages/{sub.nested.nested.nested}"
+               |> Transcode.tokenize()
+               |> Transcode.parse([], [])
+    end
+
+    test "can parse multiple-bindings with field paths " do
+      assert {[first: ["subfield"], second: ["subfield"]],
+              ["v1", "messages", {:first, []}, {:second, []}]} ==
+               "/v1/messages/{first.subfield}/{second.subfield}"
+               |> Transcode.tokenize()
+               |> Transcode.parse([], [])
+    end
+  end
+
+  defp build_simple_rule(method, pattern) do
+    Google.Api.HttpRule.new(pattern: {method, pattern})
+  end
+
+  # rule = Google.Api.HttpRule.new(pattern: {:get, "/v1/{name=messages/*}"})
+end
