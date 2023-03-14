321fb7f70e6c709dc656d961196083d9263079fa
Noah Betzen <noah@nezteb.net>
2023-03-14 05:04:54 -0700

    Add support for ranch_tcp opts: ip, ipv6_v6only, inet, inet6 (#308)
    * feat: add support for ranch_tcp socket opts
    
    * Update lib/grpc/server/adapters/cowboy.ex
    
    * Update lib/grpc/server/adapters/cowboy.ex
    
    * Update Elixir versions for asdf and mix project
    
    * Format
    
    * Update mix.exs
    
    ---------
    
    Co-authored-by: Paulo Valente <16843419+polvalente@users.noreply.github.com>

--- lib/grpc/server/adapters/cowboy.ex
+++ lib/grpc/server/adapters/cowboy.ex
@@ -221,7 +221,14 @@ defmodule GRPC.Server.Adapters.Cowboy do
 
   defp socket_opts(port, opts) do
     socket_opts = [port: port]
-    socket_opts = if opts[:ip], do: [{:ip, opts[:ip]} | socket_opts], else: socket_opts
+
+    # https://ninenines.eu/docs/en/ranch/1.7/manual/ranch_tcp/
+    socket_opts =
+      Enum.reduce(opts, socket_opts, fn
+        {k, v}, acc when k in [:ip, :ipv6_v6only] and not is_nil(v) -> [{k, v} | acc]
+        {:net, v}, acc when not is_nil(v) -> [v | acc]
+        _, acc -> acc
+      end)
 
     if opts[:cred] do
       opts[:cred].ssl ++
