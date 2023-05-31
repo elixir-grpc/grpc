4b779a4d62fe9a15f014b0007256bc041a70a60b
Diego Camacho <hackvan@gmail.com>
2023-05-31 16:05:58 -0500

    Test: Refactor tests related to the expected status code and expected error message using a common factory function.

--- test/grpc/rpc_error_test.exs
+++ test/grpc/rpc_error_test.exs
@@ -1,35 +1,45 @@
 defmodule GRPC.RPCErrorTest do
-  use ExUnit.Case, async: true
+  use GRPC.DataCase, async: true
 
   import GRPC.RPCError, only: [is_rpc_error: 2]
 
+  alias GRPC.Status
   alias GRPC.RPCError
 
   describe "is_rpc_error/2 guard clause" do
     test "returns true for GRPC.RPCError structs with same status" do
       exception = GRPC.RPCError.new(:unknown)
 
-      assert is_rpc_error(exception, 2)
+      assert is_rpc_error(exception, Status.unknown())
     end
 
     test "returns false for GRPC.RPCError structs with different status" do
       exception = GRPC.RPCError.new(:unknown)
 
-      refute is_rpc_error(exception, 1)
+      refute is_rpc_error(exception, Status.cancelled())
     end
 
     test "returns false for others types of input" do
       exception = %{status: :unknown}
 
-      refute is_rpc_error(exception, 2)
+      refute is_rpc_error(exception, Status.unknown())
     end
   end
 
   describe "new/1" do
-    test "creates GRPC.RPCError struct with a default message" do
+    test "creates the GRPC.RPCError struct with a default message" do
       assert %RPCError{status: code, message: message} = RPCError.new(:unknown)
-      assert code == 2
+      assert code == Status.unknown()
       refute is_nil(message)
+    end
+
+    test "creates the GRPC.RPCError struct with its own predefined message and status code" do
+      for {name, expected_code, expected_message} <- grpc_status_codes_messages_factory(),
+          not is_nil(expected_message) do
+        assert %RPCError{status: code, message: message} = RPCError.new(name)
+        assert expected_code == code
+        assert expected_message == message
+      end
     end
   end
 end
--- test/grpc/status_test.exs
+++ test/grpc/status_test.exs
@@ -1,30 +1,10 @@
 defmodule GRPC.StatusTest do
-  use ExUnit.Case, async: true
+  use GRPC.DataCase, async: true
 
   alias GRPC.Status
 
-  @grpc_status_codes %{
-    ok: 0,
-    cancelled: 1,
-    unknown: 2,
-    invalid_argument: 3,
-    deadline_exceeded: 4,
-    not_found: 5,
-    already_exists: 6,
-    permission_denied: 7,
-    resource_exhausted: 8,
-    failed_precondition: 9,
-    aborted: 10,
-    out_of_range: 11,
-    unimplemented: 12,
-    internal: 13,
-    unavailable: 14,
-    data_loss: 15,
-    unauthenticated: 16
-  }
-
   test "match an error atom status to a gRPC status defined code" do
-    for {function, expected_value} <- @grpc_status_codes,
-        do: assert(expected_value == apply(Status, function, []))
+    for {function, expected_code, _expected_message} <- grpc_status_codes_messages_factory(),
+        do: assert(expected_code == apply(Status, function, []))
   end
 end
