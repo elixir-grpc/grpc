da06250e6a498b7a38f47e33ad5f6b69847fcf5d
Adriano Santos <sleipnir@bsd.com.br>
2024-07-13 10:57:05 -0300

    Update version and maintainers

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule GRPC.Mixfile do
   use Mix.Project
 
-  @version "0.7.0"
+  @version "0.8.2"
 
   def project do
     [
@@ -63,7 +63,7 @@ defmodule GRPC.Mixfile do
 
   defp package do
     %{
-      maintainers: ["Bing Han", "Paulo Valente"],
+      maintainers: ["Bing Han", "Paulo Valente", "Dave Lucia", "Adriano Santos"],
       licenses: ["Apache 2"],
       links: %{"GitHub" => "https://github.com/elixir-grpc/grpc"},
       files: ~w(mix.exs README.md lib src config LICENSE .formatter.exs)
