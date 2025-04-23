a22f2fab8a0a16f9790c514c35f4725fbbb97c78
Aaron Seigo <aseigo@kde.org>
2025-04-23 17:37:17 +0200

    another test: if there is NO sec-fetch-mode header

--- test/grpc/server/interceptors/cors_test.exs
+++ test/grpc/server/interceptors/cors_test.exs
@@ -158,9 +158,25 @@ defmodule GRPC.Server.Interceptors.CORSTest do
         http_request_headers: Map.put(@default_http_headers, "sec-fetch-mode", "same-origin")
     }
 
-    # fetch the interceptor state from the fake endpoint
-    [{_interceptor, interceptor_state}] =
-      GRPC.Server.Interceptors.CORSTest.Endpoint.__meta__(:interceptors)
+    {:ok, :ok} =
+      CORSInterceptor.call(
+        request,
+        %{stream | access_mode: :grpcweb},
+        fn _request, _stream -> {:ok, :ok} end,
+        CORSInterceptor.init(allow: "*")
+      )
+
+    refute_received({:setting_headers, _}, "Set CORS header")
+  end
+
+  test "No CORS if missing sec-fetch-mode header" do
+    request = %FakeRequest{}
+
+    stream = %{
+      create_stream()
+      | access_mode: :grpcweb,
+        http_request_headers: Map.delete(@default_http_headers, "sec-fetch-mode")
+    }
 
     {:ok, :ok} =
       CORSInterceptor.call(
