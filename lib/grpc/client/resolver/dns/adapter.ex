e4eee2dcc4412391ef1ef5abde2f57f5100256f8
sleipnir <solid.sistemas@gmail.com>
2025-09-26 13:03:47 -0300

    accept string

--- lib/grpc/client/resolver/dns/adapter.ex
+++ lib/grpc/client/resolver/dns/adapter.ex
@@ -3,10 +3,10 @@ defmodule GRPC.Client.Resolver.DNS.Adapter do
   Adapter to resolve DNS (A and TXT).
   """
 
-  @callback lookup(charlist(), :a | :txt) ::
+  @callback lookup(String.t(), :a | :txt) ::
               {:ok, [tuple() | String.t()]} | {:error, term()}
 
   def lookup(name, type) do
-    :inet_res.lookup(name, :in, type)
+    :inet_res.lookup(String.to_charlist(name), :in, type)
   end
 end
