2fb38111319dc901bd7c339558e644f07a4456d9
Adriano Santos <solid.sistemas@gmail.com>
2025-11-13 16:07:15 -0300

    Update lib/grpc/client/supervisor.ex

--- lib/grpc/client/supervisor.ex
+++ lib/grpc/client/supervisor.ex
@@ -45,8 +45,11 @@ defmodule GRPC.Client.Supervisor do
 
   def start_link(opts) do
     case DynamicSupervisor.start_link(__MODULE__, opts, name: __MODULE__) do
-      {:error, {:already_started, pid}} -> {:ok, pid}
-      {:ok, _pid} = started -> started
+      {:ok, _pid} = started -> 
+        started
+        
+      {:error, {:already_started, pid}} -> 
+        {:ok, pid}
       other -> other
     end
   end
