692e8db3df1c50128cc66721e6bc31ba16f497be
Adriano Santos <solid.sistemas@gmail.com>
2025-12-12 12:11:57 -0300

    Update benchmark/config/config.exs
    Co-authored-by: Paulo Valente <16843419+polvalente@users.noreply.github.com>

--- benchmark/config/config.exs
+++ benchmark/config/config.exs
@@ -1,6 +1,6 @@
 import Config
 
-# Disable logging by default for better performance
+# Reduce logging overhead by default for better performance
 config :logger, level: :error
 
 import_config "#{Mix.env()}.exs"
