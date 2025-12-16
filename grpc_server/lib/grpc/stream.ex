3c8fc4294e02a10a931765f2d4b34c83be76b6df
Adriano Santos <adriano.santos@v3.com.br>
2025-12-16 12:47:33 -0300

    fix: map_error send_response

--- grpc_server/lib/grpc/stream.ex
+++ grpc_server/lib/grpc/stream.ex
@@ -542,7 +542,12 @@ defmodule GRPC.Stream do
     dry_run? = Keyword.get(opts, :dry_run, false)
 
     if not dry_run? do
-      GRPC.Server.send_reply(from, msg)
+      # RPCError should be raised, not sent as reply
+      case msg do
+        %GRPC.RPCError{} = rpc_error -> raise rpc_error
+        {:error, %GRPC.RPCError{} = rpc_error} -> raise rpc_error
+        _ -> GRPC.Server.send_reply(from, msg)
+      end
     end
   end
 end
