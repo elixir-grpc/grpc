aee346b5910b19e17ad12ad681d65a539da4380c
Kalle Päplow <kalle.paeplow@gmail.com>
2026-02-09 15:14:24 +0100

    added test

--- grpc_client/test/grpc/resolver/dns_test.exs
+++ grpc_client/test/grpc/resolver/dns_test.exs
@@ -49,4 +49,46 @@ defmodule GRPC.Client.Resolver.DNSTest do
     assert {"foo", "bar"} in method_names
     assert {"baz", nil} in method_names
   end
+
+  test "resolves AAAA record and parses service config from TXT via GenServer" do
+    host = "my-service.local"
+    config_name = "_grpc_config." <> host
+
+    DNS.MockAdapter
+    |> expect(:lookup, fn ^host, :a ->
+      {:ok, []}
+    end)
+    |> expect(:lookup, fn ^host, :aaaa ->
+      {:ok, [{0, 0, 0, 0, 0, 0, 0, 1}]}
+    end)
+    |> expect(:lookup, fn ^config_name, :txt ->
+      {:ok,
+       [
+         ~c'grpc_config={
+           "loadBalancingConfig":[{"round_robin":{}}],
+           "methodConfig":[
+             {
+               "name":[
+                 {"service":"foo","method":"bar"},
+                 {"service":"baz"}
+               ],
+               "timeout":"1.000000001s"
+             }
+           ]
+         }'
+       ]}
+    end)
+
+    assert {:ok, %{addresses: addrs, service_config: config}} = DNS.resolve(host)
+    assert [%{address: "::1", port: 50051}] = addrs
+    assert config.load_balancing_policy == :round_robin
+
+    method_names =
+      Enum.flat_map(config.method_configs, fn mc ->
+        Enum.map(mc["name"], fn n -> {n["service"], Map.get(n, "method")} end)
+      end)
+
+    assert {"foo", "bar"} in method_names
+    assert {"baz", nil} in method_names
+  end
 end
