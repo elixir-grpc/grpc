b3702825a2986b1c6437e570fed31ea896a9ffd4
Adriano Santos <adriano.santos@v3.com.br>
2026-03-13 17:18:00 -0300

    chore: set hex version

--- grpc_client/mix.exs
+++ grpc_client/mix.exs
@@ -29,8 +29,8 @@ defmodule GrpcClient.MixProject do
 
   defp deps do
     [
-      {:grpc_core, path: "../grpc_core"},
-      # {:grpc_core, "~> 1.0"}, # Uncomment for hex release
+      #{:grpc_core, path: "../grpc_core"},
+      {:grpc_core, "~> 1.0"}, # Uncomment for hex release
       {:gun, "~> 2.0"},
       {:mint, "~> 1.5"},
       {:castore, "~> 0.1 or ~> 1.0", optional: true},
